64fd98b376f2bdf1432488e99b6a1eb891f1b6cd
Raphael <raphael286@proton.me>
2026-04-07 20:29:24 -0400

    renamed

--- Raphael.Lazulite/AcceleratedObject.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Raphael.Lazulite;
-
-public class AcceleratedObject
-{
-    
-}
---
+++ Raphael.Lazulite/LazuliteBase.cs
@@ -0,0 +1,33 @@
+﻿using ILGPU;
+using ILGPU.Runtime;
+
+namespace Raphael.Lazulite;
+
+public abstract class LazuliteBase<TElement, THost>(MemoryBuffer1D<TElement, Stride1D.Dense> buffer, BufferPool<TElement> pool) : IDisposable
+    where TElement : unmanaged 
+    where THost : notnull
+{
+    public MemoryBuffer1D<TElement, Stride1D.Dense> Buffer { get; } = buffer;
+    public bool NotDisposable { get; set; } = false;
+    public bool Disposed { get; private set; } = false;
+
+    readonly protected BufferPool<TElement> _pool = pool;
+    protected Action? _disposeHook;
+
+    public THost ToHost()
+    {
+        _pool._lctx.Accelerator.Synchronize();
+        return Convert(Buffer.GetAsArray1D());
+    }
+
+    public void Dispose()
+    {
+        if (NotDisposable) return;
+        _disposeHook?.Invoke();
+        _pool.Return();
+        Disposed = true;
+    }
+
+    public abstract THost Convert(TElement[] raw);
+    
+}
