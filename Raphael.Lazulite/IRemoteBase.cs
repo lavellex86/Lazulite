ee753fd30a6e0c98fb9bcae8f60bc225a844a0a5
Raphael <raphael286@proton.me>
2026-05-07 08:44:40 -0400

    2.0.1, IRemoteBase and IRemoteTensor

---
+++ Raphael.Lazulite/IRemoteBase.cs
@@ -0,0 +1,16 @@
+﻿using ILGPU;
+using ILGPU.Runtime;
+
+namespace Raphael.Lazulite;
+
+public interface IRemoteBase<TElement> : IDisposable
+    where TElement : unmanaged
+{
+    public MemoryBuffer1D<TElement, Stride1D.Dense> Buffer { get; }
+    public bool NotDisposable { get; set; }
+    public bool Disposed { get; }
+    
+    public int IntLength { get; }
+    public BufferPool<TElement> Pool { get; }
+    public LazuliteContext Context { get; }
+}
---
+++ Raphael.Linalg32/IRemoteTensor.cs
@@ -0,0 +1,8 @@
+﻿using Raphael.Lazulite;
+
+namespace Raphael.Linalg32;
+
+public interface IRemoteTensor : IRemoteBase<float>
+{
+    public int[] Shape { get; }
+}
