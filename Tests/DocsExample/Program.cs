39aa1c69fd7039921144b5c30dc5a2a0f581d2db
raphael286 <raphael286@proton.me>
2026-07-31 09:12:30 -0400

    docs example

---
+++ Tests/DocsExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using ILGPU;
+using ILGPU.Runtime;
+using Raphael.Lazulite;
+
+var lctx = new LazuliteContext(); 
+var pool = new BufferPool<int>(lctx);
+
+var remote = new RemoteIntArray(pool.Get(3), pool) 
+    .Set([1, 2, 3]);
+
+var kernel = new LazuliteKernel<Action<Index1D, ArrayView1D<int, Stride1D.Dense>>>((i, arr) => arr[i] += 1, lctx);
+kernel.Call(3, remote.Buffer);
+
+var returned = remote.Get();
+Console.WriteLine($"[{returned[0]}, {returned[1]}, {returned[2]}]");
+
+class RemoteIntArray(MemoryBuffer1D<int, Stride1D.Dense> buffer, BufferPool<int> pool) 
+    : RemoteBase<int, int[]>(buffer, pool) 
+{
+    protected override int[] ConvertToHost(int[] raw) => raw;
+    protected override int[] ConvertToRaw(int[] host) => host;
+}
